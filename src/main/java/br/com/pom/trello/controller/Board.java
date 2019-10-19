d4361ce04a6dcde51718a1e083cb4ae1757aa54b
de Medeiros Lobo <rslo@gft.com>
2019-10-19 13:49:11 -0300

    Medoto de criação do board criado e testato

--- src/main/java/br/com/pom/trello/controller/Board.java
+++ src/main/java/br/com/pom/trello/controller/Board.java
@@ -2,23 +2,20 @@ package br.com.pom.trello.controller;
 
 import br.com.pom.trello.interfaces.Credentials;
 import br.com.pom.trello.interfaces.Environment;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class Board implements Environment, Credentials {
 
-    private Response response;
-
     public String criarQuadro(String nome){
-
         RequestSpecification httpRequest = given();
-        response = httpRequest.post("");
-
-        return "";
+        httpRequest.header("content-type","application/json; charset=utf-8");
+        Response response = httpRequest.post(URL_BASE + "/1/boards/?name=" + nome + "&key=" + KEY + "&token=" + TOKEN);
+        return response.getBody().jsonPath().get("id");
     }
-
-
 
 }
---
+++ src/main/java/br/com/pom/trello/controller/List.java
@@ -0,0 +1,8 @@
+package br.com.pom.trello.controller;
+
+import br.com.pom.trello.interfaces.Credentials;
+import br.com.pom.trello.interfaces.Environment;
+
+public class List implements Environment, Credentials {
+    
+}
